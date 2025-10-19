5d27a9962ae3211d279af7b71ab2900d07173398
santiagobedon <santiago.bedon@correounivalle.edu.co>
2025-10-19 18:34:15 -0500

    correcion de conexion con el back

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // src/services/api.ts
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://dasakemovies.onrender.com/api";
+const API_BASE_URL = "https://dasakemovies.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
