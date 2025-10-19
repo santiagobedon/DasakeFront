6332dcea2b04baa50941478d64733fe5f1ae1183
santiagobedon <santiago.bedon@correounivalle.edu.co>
2025-10-19 17:00:37 -0500

    correcion de algunas variables

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import "./Dashboard.scss";
